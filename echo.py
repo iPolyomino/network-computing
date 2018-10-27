4393aa40ee7303ba7097dde0a1ca1032720a4070
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2018-10-27 16:17:10 +0900

    fix indent

--- echo.py
+++ echo.py
@@ -5,29 +5,29 @@ import argparse
 
 
 def main():
-        args = get_args()
-        req_url = 'ws://{}'.format(args.host)
-        print('Connect to {}'.format(req_url))
-        ws = create_connection(req_url)
+    args = get_args()
+    req_url = 'ws://{}'.format(args.host)
+    print('Connect to {}'.format(req_url))
+    ws = create_connection(req_url)
 
-        if args.message:
-                ws.send(args.message)
-                print(ws.recv())
+    if args.message:
+        ws.send(args.message)
+        print(ws.recv())
 
-        print('Close')
-        ws.close()
+    print('Close')
+    ws.close()
 
 
 def get_args():
-        parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
-        parser.add_argument('--message', type=str)
-        parser.add_argument('--host', type=str, default='localhost:8080')
+    parser.add_argument('--message', type=str)
+    parser.add_argument('--host', type=str, default='localhost:8080')
 
-        args = parser.parse_args()
+    args = parser.parse_args()
 
-        return args
+    return args
 
 
 if __name__ == '__main__':
-        main()
+    main()
